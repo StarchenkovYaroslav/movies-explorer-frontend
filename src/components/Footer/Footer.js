2251abdd03303348428761dfd6f49a5a53f92046
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2022-03-07 03:56:26 +0700

    feat: сверстать футер

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,19 @@
+import "./Footer.css";
+
 function Footer() {
-  return null;
+  const currentYear = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <p className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</p>
+      <p className="footer__year">&copy; {currentYear}</p>
+      <nav className="footer__navigation">
+        <a href="https://practicum.yandex.ru" className="footer__link">Яндекс.Практикум</a>
+        <a href="https://github.com/" className="footer__link">Github</a>
+        <a href="https://facebook.com/" className="footer__link">Facebook</a>
+      </nav>
+    </footer>
+  );
 }
 
 export default Footer;
