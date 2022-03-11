99515c62829f29a46e57692f307880eff5d5bdd3
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2022-03-12 01:06:35 +0700

    refactor: вынести логотип в компонент Logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,21 +1,14 @@
 import "./Header.css";
 
-import { Link } from "react-router-dom";
-
-import { paths } from "../../utils/config";
-
-import headerLogo from "../../images/header__logo.svg";
-
 import Navigation from "../Navigation/Navigation";
+import Logo from "../Logo/Logo";
 
 function Header({ loggedIn }) {
   const headerClassName = loggedIn ? 'header  header_logged-in' : 'header';
 
   return (
     <header className={headerClassName}>
-      <Link to={paths.main}>
-        <img className="header__logo" src={headerLogo} alt="логотип" />
-      </Link>
+      <Logo />
       <Navigation loggedIn={loggedIn} />
     </header>
   )
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import "./Logo.css";
+
+import {Link} from "react-router-dom";
+
+import {paths} from "../../utils/config";
+
+import headerLogo from "../../images/header__logo.svg";
+
+function Logo() {
+  return (
+    <Link to={paths.main}>
+      <img className="logo" src={headerLogo} alt="логотип" />
+    </Link>
+  )
+}
+
+export default Logo;
