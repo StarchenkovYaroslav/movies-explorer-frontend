d10a93afe9878d049595d1a0085f1e1ca834df27
Yaroslav Starchenkov <starchenkov.ya@gmail.com>
2022-03-18 01:04:13 +0700

    feat: добавить валидацию формы регистрации

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -1,34 +1,60 @@
 import Form from "../Form/Form";
 import Input from "../Input/Input";
+import {useFormWithValidation} from "../../utils/hooks/use-form-with-validation";
 
 function RegisterForm(props) {
+  const {
+    inputValues,
+    inputsValidity,
+    inputErrorMessages,
+    isFormValid,
+    handleInputChange,
+    resetForm
+  } = useFormWithValidation(
+    {name: '', email: '', password: ''},
+    {name: true, email: true, password: ''},
+    {name: '', email: '', password: ''},
+    false
+  );
+
   return (
     <Form
       submitButtonText="Зарегистрироваться"
+      isValid={isFormValid}
     >
       <Input
+        required={true}
         label="Имя"
         name="name"
         type="text"
         placeholder="Введите имя"
         id="profile-name"
-        validationMessage="аяяй"
+        onChange={handleInputChange}
+        value={inputValues.name}
+        validationMessage={inputErrorMessages.name}
+
       />
       <Input
+        required={true}
         label="E-mail"
         name="email"
         type="email"
         placeholder="Введите email"
         id="profile-email"
-        validationMessage="аяяй"
+        onChange={handleInputChange}
+        value={inputValues.email}
+        validationMessage={inputErrorMessages.email}
       />
       <Input
+        required={true}
         label="Пароль"
         name="password"
         type="password"
         placeholder="Введите пароль"
         id="profile-password"
-        validationMessage="аяяй"
+        onChange={handleInputChange}
+        value={inputValues.password}
+        validationMessage={inputErrorMessages.password}
       />
     </Form>
   )
